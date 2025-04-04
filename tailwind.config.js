7b48af3ba051919062edf93e67534ccfff8a748b
Berk Yuriev <berk2345@abv.bg>
2025-04-04 13:15:22 +0300

    Project added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    colors: {
+      'primary': '#FFCF02',
+      'secondary': '#173158',
+      'indigo-lighter': '#b3bcf5',
+      'indigo': '#5c6ac4',
+      'indigo-dark': '#202e78',
+    }
+  }
+}

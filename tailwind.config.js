cbb1701bddee7f72ae9fad410657e9c76cf54bda
Berk Yuriev <berk2345@abv.bg>
2025-04-12 17:29:29 +0300

    fixed font size button duration added checkboxes styled

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
 module.exports = {
-  theme: {
-    colors: {
-      'primary': '#FFCF02',
-      'secondary': '#173158',
-      'indigo-lighter': '#b3bcf5',
-      'indigo': '#5c6ac4',
-      'indigo-dark': '#202e78',
-    }
-  }
-}
+	theme: {
+		colors: {
+			primary: "#FFCF02",
+			secondary: "#173158",
+			"indigo-lighter": "#b3bcf5",
+			indigo: "#5c6ac4",
+			"indigo-dark": "#202e78",
+		},
+	},
+};
+/** @type {import('tailwindcss').Config} */
+export default {
+	theme: {
+		extend: {
+			// ...
+		},
+	},
+	plugins: [require("@tailwindcss/forms")],
+};
+module.exports = {
+	content: ["./src/**/*.{html,js}"], // Adjust paths to match your project structure
+	theme: {
+		extend: {
+			backgroundImage: {
+				"tick-image": "url('"\Users\trash\Desktop\CampaingMeister\dist\images\Property 1=active.png')", // Make sure the path is correct
+			},
+		},
+	},
+	plugins: [],
+};
